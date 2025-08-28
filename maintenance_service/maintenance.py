1cb376989a7035da31c1d40f6238f16027b456b3
Jonas Werner <werner-jonas@outlook.com>
2025-08-28 18:54:06 +0200

    catch generic exceptions

--- maintenance_service/maintenance.py
+++ maintenance_service/maintenance.py
@@ -60,60 +60,65 @@ if __name__ == "__main__":
 
     database = WeatherDatabase()
 
-    logger.info("Starting daily maintenance job...")
+    try:
+        logger.info("Starting daily maintenance job...")
 
-    TODAY = date.today()
+        TODAY = date.today()
 
-    rollover_date = TODAY - timedelta(days=2)
+        rollover_date = TODAY - timedelta(days=2)
 
-    logger.info(f"Rollover date is: {rollover_date}")
+        logger.info(f"Rollover date is: {rollover_date}")
 
-    config = OpenMeteoClientConfig(
-        create_from_file=True,
-        kwargs={"history_start_date": rollover_date},
-    )
-
-    ArchiveClient = OpenMeteoArchiveClient(config)
-    historic_data_daily = ArchiveClient.main()
-    history_orm_objects_daily = database.create_orm_objects(
-        data=historic_data_daily, table=DailyWeatherHistory
-    )
-
-    database.truncate_table(DailyWeatherForecast)
-
-    ForecastClient = OpenMeteoForecastClient(config)
-    forecast_data_daily = ForecastClient.main()
-    forecast_orm_objects_daily = database.create_orm_objects(
-        data=forecast_data_daily, table=DailyWeatherForecast
-    )
-
-    database.write_data(history_orm_objects_daily)
-    database.write_data(forecast_orm_objects_daily)
-
-    logger.info("Daily maintenance routine completed successfully!")
-
-    if TODAY.weekday() == 0:
-        logger.info("Starting weekly maintenance job...")
-
-        rollover_year = (TODAY - timedelta(days=1)).year
-        rollover_week = (TODAY - timedelta(days=1)).isocalendar().week
-
-        logger.info(f"Rollover week is: {rollover_week}-{rollover_year}")
-
-        database.rollover_weekly_data(rollover_year, rollover_week)
-
-        forecast_data_weekly, _, _ = WeeklyTableConstructor().main(forecast_data_daily)
-        forecast_data_weekly["source"] = "Open Meteo"
-        forecast_orm_objects_weekly = database.create_orm_objects(
-            data=forecast_data_weekly, table=WeeklyWeatherForecast
+        config = OpenMeteoClientConfig(
+            create_from_file=True,
+            kwargs={"history_start_date": rollover_date},
         )
 
-        database.write_data(forecast_orm_objects_weekly)
+        ArchiveClient = OpenMeteoArchiveClient(config)
+        historic_data_daily = ArchiveClient.main()
+        history_orm_objects_daily = database.create_orm_objects(
+            data=historic_data_daily, table=DailyWeatherHistory
+        )
 
-        logger.info("Weekly maintenance routine completed successfully!")
-    else:
-        logger.info("Skipping weekly maintenance job...")
+        database.truncate_table(DailyWeatherForecast)
 
-    database.close()
+        ForecastClient = OpenMeteoForecastClient(config)
+        forecast_data_daily = ForecastClient.main()
+        forecast_orm_objects_daily = database.create_orm_objects(
+            data=forecast_data_daily, table=DailyWeatherForecast
+        )
 
-    logger.info("Maintenance routine completed successfully!")
+        database.write_data(history_orm_objects_daily)
+        database.write_data(forecast_orm_objects_daily)
+
+        logger.info("Daily maintenance routine completed successfully!")
+
+        if TODAY.weekday() == 0:
+            logger.info("Starting weekly maintenance job...")
+
+            rollover_year = (TODAY - timedelta(days=1)).year
+            rollover_week = (TODAY - timedelta(days=1)).isocalendar().week
+
+            logger.info(f"Rollover week is: {rollover_week}-{rollover_year}")
+
+            database.rollover_weekly_data(rollover_year, rollover_week)
+
+            forecast_data_weekly, _, _ = WeeklyTableConstructor().main(
+                forecast_data_daily
+            )
+            forecast_data_weekly["source"] = "Open Meteo"
+            forecast_orm_objects_weekly = database.create_orm_objects(
+                data=forecast_data_weekly, table=WeeklyWeatherForecast
+            )
+
+            database.write_data(forecast_orm_objects_weekly)
+
+            logger.info("Weekly maintenance routine completed successfully!")
+        else:
+            logger.info("Skipping weekly maintenance job...")
+
+        logger.info("Maintenance routine completed successfully!")
+    except Exception as e:
+        logger.error(e)
+    finally:
+        database.close()
