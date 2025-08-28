8858146693b44aadcaa74cc9db6262ab55eae699
Jonas Werner <werner-jonas@outlook.com>
2025-08-28 18:48:04 +0200

    catch generic exceptions

--- bootstrap_service/bootstrap.py
+++ bootstrap_service/bootstrap.py
@@ -72,54 +72,59 @@ if __name__ == "__main__":
 
     database = WeatherDatabase()
 
-    if database.bootstrap:
-        logger.info("Starting bootstrap routine...")
+    try:
+        if database.bootstrap:
+            logger.info("Starting bootstrap routine...")
 
-        database.create_tables()
-        database.truncate_table(DailyWeatherHistory)
-        database.truncate_table(DailyWeatherForecast)
-        database.truncate_table(WeeklyWeatherHistory)
-        database.truncate_table(WeeklyWeatherForecast)
+            database.create_tables()
+            database.truncate_table(DailyWeatherHistory)
+            database.truncate_table(DailyWeatherForecast)
+            database.truncate_table(WeeklyWeatherHistory)
+            database.truncate_table(WeeklyWeatherForecast)
 
-        config = OpenMeteoClientConfig(create_from_file=True)
+            config = OpenMeteoClientConfig(create_from_file=True)
 
-        ArchiveClient = OpenMeteoArchiveClient(config)
-        historic_data_daily = ArchiveClient.main()
-        history_orm_objects_daily = database.create_orm_objects(
-            data=historic_data_daily, table=DailyWeatherHistory
-        )
-
-        ForecastClient = OpenMeteoForecastClient(config)
-        forecast_data_daily = ForecastClient.main()
-        forecast_orm_objects_daily = database.create_orm_objects(
-            data=forecast_data_daily, table=DailyWeatherForecast
-        )
-
-        historic_data_weekly, _, historic_data_daily_tail = (
-            WeeklyTableConstructor().main(historic_data_daily)
-        )
-        history_orm_objects_weekly = database.create_orm_objects(
-            data=historic_data_weekly, table=WeeklyWeatherHistory
-        )
-
-        forecast_data_weekly, _, _ = WeeklyTableConstructor().main(
-            pd.concat(
-                [historic_data_daily_tail, forecast_data_daily], axis=0, join="outer"
+            ArchiveClient = OpenMeteoArchiveClient(config)
+            historic_data_daily = ArchiveClient.main()
+            history_orm_objects_daily = database.create_orm_objects(
+                data=historic_data_daily, table=DailyWeatherHistory
             )
-        )
-        forecast_data_weekly["source"] = "Open Meteo"
-        forecast_orm_objects_weekly = database.create_orm_objects(
-            data=forecast_data_weekly, table=WeeklyWeatherForecast
-        )
 
-        database.write_data(history_orm_objects_daily)
-        database.write_data(forecast_orm_objects_daily)
-        database.write_data(history_orm_objects_weekly)
-        database.write_data(forecast_orm_objects_weekly)
+            ForecastClient = OpenMeteoForecastClient(config)
+            forecast_data_daily = ForecastClient.main()
+            forecast_orm_objects_daily = database.create_orm_objects(
+                data=forecast_data_daily, table=DailyWeatherForecast
+            )
 
-        logger.info("Bootstrap routine completed successfully!")
+            historic_data_weekly, _, historic_data_daily_tail = (
+                WeeklyTableConstructor().main(historic_data_daily)
+            )
+            history_orm_objects_weekly = database.create_orm_objects(
+                data=historic_data_weekly, table=WeeklyWeatherHistory
+            )
 
-    else:
-        logger.info("Tables already exist. Skipping bootstrap routine...")
+            forecast_data_weekly, _, _ = WeeklyTableConstructor().main(
+                pd.concat(
+                    [historic_data_daily_tail, forecast_data_daily],
+                    axis=0,
+                    join="outer",
+                )
+            )
+            forecast_data_weekly["source"] = "Open Meteo"
+            forecast_orm_objects_weekly = database.create_orm_objects(
+                data=forecast_data_weekly, table=WeeklyWeatherForecast
+            )
 
-    database.close()
+            database.write_data(history_orm_objects_daily)
+            database.write_data(forecast_orm_objects_daily)
+            database.write_data(history_orm_objects_weekly)
+            database.write_data(forecast_orm_objects_weekly)
+
+            logger.info("Bootstrap routine completed successfully!")
+
+        else:
+            logger.info("Tables already exist. Skipping bootstrap routine...")
+    except Exception as e:
+        logger.error(e)
+    finally:
+        database.close()
